ee238f8af69229c9e63eb01c819f9f439494c747
Dipakkandel-2004 <137183953+Dipakkandel-2004@users.noreply.github.com>
2024-04-08 08:27:54 +0545

    Add files via upload

---
+++ clock/main.js
@@ -0,0 +1,15 @@
+let Hour = document.querySelector(".Hour");
+let Minute = document.querySelector(".Minute");
+let Second = document.querySelector(".Second");
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hroatation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+    Hour.style.transform = `rotate(${hroatation}deg)`;
+    Minute.style.transform = `rotate(${mrotation}deg)`;
+    Second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
